aa8f88a00d912b36c81a32f4a977d3a7485555a0
Chandraleksha <115769666+Chandraleksha@users.noreply.github.com>
2023-04-20 12:14:14 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -1,1 +1,9 @@
+class Fraction:
+    def__init__(self, numerator, denominator):
+       self.numerator = numerator
+       self.denominator = denominator
 
+    def add(self,other):
+        common_denominator = self.denominator * other.denominator
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        return fraction(new_numerator, common_denominator)
